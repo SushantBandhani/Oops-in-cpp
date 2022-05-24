b8413141295b6608afac5e1e708a61ca5d2fa233
Sushant <101317113+SushantBandhani@users.noreply.github.com>
2022-05-24 17:02:45 +0530

    Adding in Friend function

---
+++ Friend function/Friend function/Friend_function14.6.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+// unary operator overloading
+class complex
+{
+    int a, b;
+
+public:
+    void setData(int x, int y)
+    {
+        a = x;
+        b = y;
+    }
+
+    void getData()
+    {
+        cout << a << " " << b;
+    }
+
+    friend complex operator-(complex);
+};
+
+complex operator-(complex c)
+{
+    complex temp;
+    temp.a = -c.a;
+    temp.b = -c.b;
+    return temp;
+}
+
+int main()
+{
+    complex c1, c2;
+    c1.setData(1, 2);
+    c2 = -c1;
+    c2.getData();
+}
